b156f9cf3b8f90a962cebad517cb4e0466db6736
Michael Bond <codemonkey8510@icloud.com>
2022-10-03 19:21:10 -0400

    Fix error

--- HelloWorldBlazor/Program.cs
+++ HelloWorldBlazor/Program.cs
@@ -8,8 +8,4 @@ builder.Services
     .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
     .AddScoped<IMessages, WasmMessages>();
 
-var app = builder.Build();
-
-app.UseStaticFiles();
-
-await app.RunAsync();
+await builder.Build().RunAsync();
